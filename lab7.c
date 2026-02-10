667a72ab2a389d1bdba50a893b70abe28e01d5f5
Thananon Sananrum <s6804062612234@email.kmutnb.ac.th>
2026-02-10 16:29:35 +0700

    Lab7no6.c: To initialize array2d without same number

--- lab7.c
+++ lab7.c
@@ -1,20 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-long convert(long h, long m, long s);
+int random100to200();
 
 
-int main(){
-    int hour, minute, second;
-    long converted;
-    scanf("%d %d %d", &hour, &minute, &second);
-    converted = convert(hour, minute, second);
-    printf("Convert to %d seconds", converted);
+int main() {
+    int matrix[20][20], rows, cols, duplicate, value;
+    srand(time(NULL));
+
+    scanf("%d %d", &rows, &cols);
+    if ((rows <= 2 || rows >= 20) || (cols <= 2 || cols >= 20)) {
+        return 0;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            duplicate = 1;
+            while (duplicate) {
+                value = random100to200();
+                duplicate = 0;
+
+                for (int r = 0; r <= i; r++) {
+                    for (int k = 0; k < (r == i ? j : cols); k++) {
+                        if (value == matrix[r][k]) {
+                            duplicate = 1;
+                        }
+                    }
+                }
+            }
+            matrix[i][j] = value;
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
 
 
-long convert(long h, long m, long s){
-    long conv;
-    conv = (h * 3600) + (m * 60) + s;
-	
-    return conv;
+int random100to200() {
+    return (rand() % 101) + 100;
 }
